4d5b41bb3ca369d32e28731f9db08dba26be3765
태우 김 <enfuenqw@outlook.kr>
2025-05-15 09:42:01 +0900

    변수의 주소와 함수에서 위치 설명

---
+++ pointer_3/pointer_11.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	int i = 777;
+	call_value(i);
+	printf("값 호출 : %d\n", i);//777
+	//icopy와 i는 값을 공유하지 않기 때문에 777
+	call_refer(&i);
+	printf("함수 호출 : %d\n",i);//888
+	//ip와 i는 주소가 같기 때문에 888
+	return 0;
+}
+
+void call_value(int icopy) {//icopy에 i의 값인 777 저장
+	icopy = 888;//icopy에 888저장
+}
+
+void call_refer(int *ip){//ip에 i의 주소 저장
+	*ip = 888;//주소가 가르키는 값
+}
